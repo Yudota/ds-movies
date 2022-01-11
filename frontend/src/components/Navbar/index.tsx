f0399ec613a7a16d13963ba10040f287720a36d8
Yudota <yuri.fernandes.961@gmail.com>
2022-01-11 02:19:00 -0300

    Navbar

---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,19 @@
+import { ReactComponent as GithubIcon } from "assets/img/github.svg";
+import "./styles.css";
+export default function Navbar() {
+  return (
+    <header>
+      <nav className="container">
+        <div className="dsmovie-nav-content">
+          <h1>DSMovie</h1>
+          <a href="https://github.com/Yudota">
+            <div className="dsmovie-contact-container">
+              <GithubIcon />
+              <p className="dsmovie-contact-link">/Yudota</p>
+            </div>
+          </a>
+        </div>
+      </nav>
+    </header>
+  );
+}
