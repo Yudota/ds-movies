37cf28f2a095e6264fd757286cf75a1394dd5996
Yudota <yuri.fernandes.961@gmail.com>
2022-01-11 23:48:20 -0300

    Navigation buttons

--- frontend/src/pages/Form/index.tsx
+++ frontend/src/pages/Form/index.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import "./styles.css";
 export default function Form() {
   const movie = {
@@ -38,7 +39,9 @@ export default function Form() {
             </button>
           </div>
         </form>
-        <button className="btn btn-primary dsmovie-btn mt-3">Cancelar</button>
+        <Link to={`/`}>
+          <button className="btn btn-primary dsmovie-btn mt-3">Cancelar</button>
+        </Link>
       </div>
     </div>
   );
